6b9c19f1a4213652b05df9d5443b0f3ec805d155
vovozona4 <140420967+vovozona4@users.noreply.github.com>
2025-08-21 11:34:12 -0300

    kh

--- main.js
+++ main.js
@@ -75,3 +75,8 @@ function classificaSenha(tamanhoAlfabeto) {
     const valorEntropia = document.querySelector('.entropia');
     valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2 ** entropia / (100e6 * 60 * 60 * 24)) + " dias para descobrir essa senha.";
 }
+
+
+
+
+
